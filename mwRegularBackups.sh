45d2bf07cb0854aa839cbde599fb67409010dca7
Martin Wientjes <MartinWientjes@users.noreply.github.com>
2017-03-26 12:43:17 +0200

    mwRegularBackups.sh

---
+++ mwRegularBackups.sh
@@ -0,0 +1,6 @@
+
+# Regular backups
+sudo rsync -aE -v --recursive --progress --times --stats /Users/martin/mw/CV/ /Users/martin/Dropbox/CV/
+sudo rsync -aE -v --recursive --progress --times --stats /Users/martin/mw/Sollicitaties/ /Users/martin/Dropbox/Sollicitaties/
+sudo rsync -aE -v --recursive --progress --times --stats /Users/martin/mw/vanedeoutplacement/ /Users/martin/Dropbox/vanedeoutplacement/
+sudo rsync -aE -v --recursive --progress --times --stats /Users/martin/mw/Coaching/ /Users/martin/Dropbox/Coaching/
