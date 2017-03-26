2c076ea02a9a9ccd038c4451c862824a625b07ee
Martin Wientjes <MartinWientjes@users.noreply.github.com>
2017-03-26 12:45:10 +0200

    Create Rsync_to_Dropbox.sh

---
+++ Rsync_to_Dropbox.sh
@@ -0,0 +1,4 @@
+sudo rsync -aE -v -update --recursive --progress --times /Users/martin/mw/Sollicitaties  /Users/martin/Dropbox
+sudo rsync -aE -v -update --recursive --progress --times /Users/martin/mw/Van\ Ede\ Outplacement  /Users/martin/Dropbox
+sudo rsync -aE -v -update --recursive --progress --times /Users/martin/mw/Scrivener  /Users/martin/Dropbox
+sudo rsync -aE -v -update --recursive --progress --times /Users/martin/mw/Developer/Scripts /Users/martin/Dropbox
